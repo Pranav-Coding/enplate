94c43e8e519f570d05efe0394cc24d929992af09
Pranav-Coding <savitakunjir7@gmail.com>
2022-07-02 07:19:14 +0530

    update

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ chmod +x enplate
 echo "copying enplate to /usr/bin/"
 sudo cp enplate /usr/bin/
 mkdir $HOME/.config/enplate
+echo "moving template files to .config/enplate"
 cp -r html $HOME/.config/enplate
 cp -r flask $HOME/.config/enplate
 
